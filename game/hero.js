96f61ce14aa9cc4c1978664402d46d90752670f2
Jeremy Laurain <jeremylaurain@gmail.com>
2015-03-27 17:56:15 +0100

    Jump gravity and move

--- game/hero.js
+++ game/hero.js
@@ -1,16 +1,43 @@
 /**
  * Created by jeremylaurain on 27/03/15.
  */
-function Hero(game) {
+function Hero(game, cursors) {
 
     this.game = game;
+    this.cursors = cursors;
 
     this.create = function() {
-        this.character = this.game.add.sprite(this.game.world.centerX, this.game.world.centerY, 'hero');
+        this.character = this.game.add.sprite(50, 480, 'hero');
+        this.character.game.physics.enable(this.character, Phaser.Physics.ARCADE);
+        this.character.anchor.set(0.5);
+        this.character.body.collideWorldBounds = true;
+        this.character.body.bounce.y = 0.05;
+        this.character.frame = 1;
     };
 
     this.update = function() {
-
+        if(this.cursors.right.isDown) {
+            console.log("go");
+            this.character.x += 5;
+            this.character.frame = 1;
+        }
+        else {
+            this.character.body.acceleration.set(0);
+        }
+        if(this.cursors.left.isDown) {
+            this.character.x -= 5;
+            this.character.frame = 0;
+        }
+        if(this.cursors.up.isDown) {
+            //JUMP
+            this.character.y -= 10;
+        }
+        else if (this.cursors.down.isDown) {
+            //ESQUIVE ?
+        }
+        else {
+            this.character.body.angularVelocity = 0;
+        }
     };
 
 }
--- game/states.js
+++ game/states.js
@@ -4,26 +4,30 @@
 var states = {
 
     theGame: {
+
         preload: function() {
             this.game.load.image('sky', 'game/images/background.png');
-            this.game.load.image('hero', 'game/images/hero.png');
+
+            this.game.load.spritesheet('hero', 'game/images/hero-white.png', 182, 144, 4);
         },
         create: function() {
             var world = this.game.world;
             var bg = this.game.add.tileSprite(0, 0, world.width, world.height, 'sky');
             //bg.autoScroll(-30, 0);
 
-            this.game.world.setBounds(0, 0, 6000, 6000);
+            this.game.world.setBounds(0, 0, 6000, 550);
+            this.game.physics.arcade.gravity.y = 500;
+            this.cursors = this.game.input.keyboard.createCursorKeys();
 
-            var hero = new Hero(this.game);
-            hero.create();
-            this.hero = hero.character;
+            this.Hero = new Hero(this.game, this.cursors);
+            this.Hero.create();
+            this.hero = this.Hero.character;
 
             this.cameraPos = new Phaser.Point(0, 0);
             this.cameraPos.setTo(this.hero.x, this.hero.y);
         },
         update: function() {
-            
+            this.Hero.update();
         }
     }
 
